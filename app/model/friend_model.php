01826e0ca3c53d583c22f4b4e9256dd1d7ac0e53
Braulio Betancourt <braulio.1796@gmail.com>
2018-05-10 18:17:10 -0500

    sendFriendRequest, rejectFriendRequest, getAllRequest, acceptFriend finalizados

--- app/model/friend_model.php
+++ app/model/friend_model.php
@@ -17,11 +17,17 @@ class FriendModel {
         $this->response = new Response();
         $this->codeErrors = json_decode(file_get_contents('http://localhost/TetrisWebService/app/codes.json'), true)['codes']['friend'];
         $this->statusRequest = [
-            'waiting' => 'Waiting',
-            'accepted' => 'Accepted'
+            'waiting' => 'WAITING',
+            'accepted' => 'ACCEPTED'
         ];
     }
 
+    /**
+     * parameters {
+     *      id_user: int
+     *      id_friend: int
+     * }
+     */
     public function sendFriendRequest($data) {
         try {
             if (isset($data['id_user'], $data['id_friend']) && $data['id_user'] != '' && $data['id_friend'] != '') {
@@ -46,9 +52,7 @@ class FriendModel {
                         )
                     );
                     $result = $sth->fetch(PDO::FETCH_OBJ);
-                    if ($result) {
-
-                    } else {
+                    if (!$result) {
                         $sql = "INSERT INTO $this->tableFriend (id_user, id_friend, request) VALUES (?, ?, ?)";
                         $sth = $this->db->prepare($sql);
                         $sth->execute(
@@ -59,6 +63,8 @@ class FriendModel {
                             )
                         );
                         $this->response->setResponse(true, 'Solicitud enviada');
+                    } else {
+                        $this->response->setResponse(false, 'Solicitud rechazada');
                     }
                 } else {
                     $this->response->setResponse(false, 'Uno de los usuarios no existe');
@@ -70,33 +76,125 @@ class FriendModel {
         return $this->response;
     }
 
+    /**
+     * parameters {
+     *      id_user: int
+     *      id_friend: int
+     * }
+     */
     public function rejectFriendRequest($data) {
         try {
-
+            if (isset($data['id_user'], $data['id_friend']) && $data['id_user'] != '' && $data['id_friend'] != '') {
+                $sql = "SELECT user.id_user, friend.id_user AS id_friend
+                        FROM user, (SELECT user.id_user FROM user WHERE user.id_user = ?) friend
+                        WHERE user.id_user = ?";
+                $sth = $this->db->prepare($sql);
+                $sth->execute(
+                    array(
+                        $data['id_friend'],
+                        $data['id_user']
+                    )
+                );
+                $result = $sth->fetch(PDO::FETCH_OBJ);
+                if ($result) {
+                    $sql = "SELECT request FROM $this->tableFriend WHERE id_user = ? AND id_friend = ?";
+                    $sth = $this->db->prepare($sql);
+                    $sth->execute(
+                        array(
+                            $data['id_user'],
+                            $data['id_friend']
+                        )
+                    );
+                    $result = $sth->fetch(PDO::FETCH_OBJ);
+                    if ($result) {
+                        if ($result->request == $this->statusRequest['waiting']) {
+                            $sql = "DELETE FROM $this->tableFriend WHERE id_user = ? AND id_friend = ?";
+                            $sth = $this->db->prepare($sql);
+                            $sth->execute(
+                                array(
+                                    $data['id_user'],
+                                    $data['id_friend']
+                                )
+                            );
+                            $this->response->setResponse(false, 'Solicitud rechazada');
+                        } else {
+                            $this->response->setResponse(false, 'No se pudo rechazar la solicitud');
+                        }
+                    } else {
+                        $this->response->setResponse(false, 'Esta solicitud de amistad no existe');
+                    }
+                } else {
+                    $this->response->setResponse(false, 'Uno de los usuarios no existe');
+                }
+            }
         } catch (Exception $e) {
             $this->response->setResponse(false, $e->getMessage());
         }
         return $this->response;
     }
 
-    public function getAllRequest($data) {
+    public function getAllRequest($id_user) {
         try {
-
+            $sql = "SELECT * FROM $this->tableFriend WHERE id_user = ? AND request = ?";
+            $sth = $this->db->prepare($sql);
+            $result = $sth->execute(
+                array(
+                    $id_user,
+                    $this->statusRequest['waiting']
+                )
+            );
+            if ($result) {
+                $this->response->setResponse(true, $sth->fetchAll());
+            }
         } catch (Exception $e) {
             $this->response->setResponse(false, $e->getMessage());
         }
         return $this->response;
     }
 
+    /**
+     * parameters {
+     *      id_user: int
+     *      id_friend: int
+     * }
+     */
     public function acceptFriend($data) {
         try {
-
+            if (isset($data['id_user'], $data['id_friend']) && $data['id_user'] != '' && $data['id_friend'] != '') {
+                $sql = "SELECT * FROM $this->tableFriend WHERE id_user = ? AND id_friend = ?";
+                $sth = $this->db->prepare($sql);
+                $sth->execute(
+                    array(
+                        $data['id_user'],
+                        $data['id_friend']
+                    )
+                );
+                $result = $sth->fetch(PDO::FETCH_OBJ);
+                if ($result->request == $this->statusRequest['waiting']) {
+                    $sql = "UPDATE $this->tableFriend SET request = ? WHERE id_user = ? AND id_friend = ?";
+                    $sth = $this->db->prepare($sql);
+                    $sth->execute(
+                        array(
+                            $this->statusRequest['accepted'],
+                            $data['id_user'],
+                            $data['id_friend']
+                        )
+                    );
+                    $this->response->setResponse(true, 'Solicitud aceptada');
+                }
+            }
         } catch (Exception $e) {
             $this->response->setResponse(false, $e->getMessage());
         }
         return $this->response;
     }
 
+    /**
+     * parameters {
+     *      id_user: int
+     *      id_friend: int
+     * }
+     */
     public function removeFriend($data) {
         try {
 
--- app/route/friend_route.php
+++ app/route/friend_route.php
@@ -17,4 +17,47 @@ $app->group('/friend/', function (){
             )
         );
     });
+
+    $this->post('rejectRequest', function ($req, $res, $args) {
+        $fm = new FriendModel();
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $fm->rejectFriendRequest(
+                    $req->getParsedBody()
+                )
+            )
+        );
+    });
+
+    $this->get('getAllRequest/{id_user}', function ($req, $res, $args) {
+        $fm = new FriendModel();
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $fm->getAllRequest($args['id_user'])
+            )
+        );
+    });
+
+    $this->post('acceptRequest', function ($req, $res, $args) {
+        $fm = new FriendModel();
+        
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $fm->acceptFriend(
+                    $req->getParsedBody()
+                )
+            )
+        );
+    });
 });
